e4ec6409217bea10882fe0e1b80e05b5f79ad2b2
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-04-27 10:26:09 +0200

    Despues de liarla volvi a ponerlo como estaba

--- src/trabajofacebook/Autorizacion.java
+++ src/trabajofacebook/Autorizacion.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package trabajofacebook;
 
 import facebook4j.Facebook;
@@ -7,8 +12,8 @@ import facebook4j.FacebookFactory;
  *
  * @author Adry
  */
-public class Autorizacion {
-    public static Facebook autorizar(){  
+class Autorizacion {
+    public static Facebook autorizar(){
     Facebook facebook = new FacebookFactory().getInstance();
     return facebook;
 }
