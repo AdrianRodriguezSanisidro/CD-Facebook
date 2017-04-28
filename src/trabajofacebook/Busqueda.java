028fa28fdfe3e2dc9ae8a65f822a71bd7066f2a9
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-04-28 15:22:38 +0200

    Nuevaclase con metodo para buscar un usuario por un mensaje

---
+++ src/trabajofacebook/Busqueda.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajofacebook;
+
+import facebook4j.FacebookException;
+import facebook4j.User;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Adry
+ */
+public class Busqueda {
+    public static void buscarUsuario() throws FacebookException{
+        String mensaje=JOptionPane.showInputDialog("Mensaje a buscar: ");
+        List<User>listaUsuario;
+        listaUsuario=Autorizacion.autorizar().getUsers(mensaje);
+        for(int i=0;i<listaUsuario.size();i++){
+            System.out.println(listaUsuario.get(i).getName());
+        }
+        
+    }
+}
